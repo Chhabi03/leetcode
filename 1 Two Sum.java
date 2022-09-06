fbb8b5050b52ca82f76b3c297dabf9c2e326740a
Chhabi03 <106945667+Chhabi03@users.noreply.github.com>
2022-09-06 09:44:41 +0530

    Update 1 Two Sum.java

--- 1 Two Sum.java
+++ 1 Two Sum.java
@@ -10,3 +10,12 @@ class Solution {
         return null;
     }
 }
+
+//Second Solution
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0) return false;
+        long x = (long) n;
+        return(x & (x-1)) == 0;
+    }
+}
